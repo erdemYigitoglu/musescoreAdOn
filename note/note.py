9e2fa4814463c390e18dafdbc958fb9a052f446e
erdem yigitoglu <erdemyigitoglu@gmail.com>
2025-03-11 21:42:15 +0300

    added method to transform note names to fixed do system

--- note/note.py
+++ note/note.py
@@ -9,3 +9,18 @@ class Note():
     @property
     def place(self):
         return self._place
+    def transformName(self):
+        if self._name[5] == "C":
+            return "do" + self._name[6:]
+        elif self._name[5] == "D":
+            return "re" + self_name[6:]
+        elif self._name[5] == "E":
+            return "me" + self._name[6:]
+        elif self._name[5] == "F":
+            return "fa" + self._name[6:]
+        elif self._name[5] == "G":
+            return "sol" + self._name[6:]
+        elif self._name[5] == "A":
+            return  "la" + self._name[6:]
+        elif self._name[5] == "B":
+            return "si" + self._name[6:]
