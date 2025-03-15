dbe77852269f032e7b96a5e01edfd6e91d3b134d
erdem yigitoglu <erdemyigitoglu@gmail.com>
2025-03-15 10:56:51 +0300

    change transform function to return full note information

--- addon/appModules/musescore4/note/note.py
+++ addon/appModules/musescore4/note/note.py
@@ -9,18 +9,18 @@ class Note():
     @property
     def place(self):
         return self._place
-    def transformName(self):
+    def transformNote(self):
         if self._name[0] == "C":
-            return "do " + self._name[6:]
+            return "do " + self._name[6:] + " , "  +self._place
         elif self._name[0] == "D":
-            return "re " + self_name[6:]
+            return "re " + self._name[6:]  + " , "  +self._place
         elif self._name[0] == "E":
-            return "me " + self._name[6:]
+            return "me " + self._name[6:] + " , "  +self._place
         elif self._name[0] == "F":
-            return "fa " + self._name[6:]
+            return "fa " + self._name[6:] + " , "  +self._place
         elif self._name[0] == "G":
-            return "sol " + self._name[6:]
+            return "sol " + self._name[6:] + " , "  +self._place
         elif self._name[0] == "A":
-            return  "la " + self._name[6:]
+            return  "la " + self._name[6:] + " , "  +self._place
         elif self._name[0] == "B":
-            return "si " + self._name[6:]
+            return "si " + self._name[6:] + " , " + self._place
