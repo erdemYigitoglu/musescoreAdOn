6a3dc6da1d949976e1640d1864aecc7fb6dc3dfd
erdem yigitoglu <erdemyigitoglu@gmail.com>
2025-03-15 13:17:34 +0300

    small change for anounce pich script to make it consistant with other scripts

--- addon/appModules/musescore4/musescore4.py
+++ addon/appModules/musescore4/musescore4.py
@@ -4,15 +4,18 @@ from scriptHandler import script
 import ui
 import api
 class AppModule(appModuleHandler.AppModule):
-    @script(
-        gesture= "kb:NVDA+shift+p",
-        description= "anounces the pich name, duration and oktav"
-    )
-    def script_anouncePich(self, gesture):
-        object = api.getFocusObject()
-        note= note.Note(object.name)
-        ui.message(note.name)
-	@script(gesture= "kb:NVDA+shift+i", description ="anounces measurement and beat information of the pich.")
+	@script(
+		gesture= "kb:NVDA+shift+p",
+		description= "anounces the pich name, duration and oktav"
+	)
+	def script_anouncePich(self, gesture):
+		object = api.getFocusObject()
+		noteInfo = note.Note(object.name)
+		ui.message(noteInfo.name)
+	@script(
+		gesture= "kb:NVDA+shift+i",
+		description ="anounces measurement and beat information of the pich."
+	)
 	def script_anounceRythim(self, gesture):
 		object = api.getFocusObject()
 		noteInfo = note.Note(object.name)
